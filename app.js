4e7bfa3d3d2886f9ffe0208a508f70ea96e3a484
roubertpi <roubertpimentar@gmail.com>
2021-01-18 22:59:36 -0300

    114 - Better Errors and Refactoing

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 /* eslint-disable prettier/prettier */
 const express = require('express');
-const { nextTick } = require('process');
-const morgan = require('morgan')
+const morgan = require('morgan');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/erroController');
+
 
 const tourRouter = require ('./routes/tourRoutes.js');
 const userRouter = require ('./routes/userRoutes.js');
@@ -16,6 +19,8 @@ if (process.env.NODE_ENV ==='development'){
 
 app.use (express.json());
 app.use (express.static(`${__dirname}/public`));
+
+
 app.use ((req,res, next)=>{
   console.log('teste ok');
   next();
@@ -25,5 +30,10 @@ app.use ((req,res, next)=>{
 app.use('/api/v1/tours',tourRouter);
 app.use('/api/v1/users',userRouter);
 
+app.all('*',(req,res,next)=>{
+  next(new AppError (`Can't find ${req.originalUrl} on this server!`));
+});
+
+app.use (globalErrorHandler);
 
 module.exports=app;
