41fc800cbe6ab7b21bc1a6655a331e1e8a177be1
roubertpi <roubertpimentar@gmail.com>
2021-01-20 01:01:06 -0300

    129- Logging in Users funcionado

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 //name, email, photo, password, passwordConfirm
 
@@ -24,15 +25,40 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, 'Digite sua Senha'],
-    minlength: [8, 'Senha de do minimo 8 Caractere']
+    minlength: [8, 'Senha de do minimo 8 Caractere'],
+    select: false,
   },
-  passwordConfirm:{
-      type:String,
-      required:[true,'Confirme sua senha'],
-      minlength: [8, 'Senha de do minimo 8 Caractere']
-  }
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Confirme sua senha'],
+    minlength: [8, 'Senha de do minimo 8 Caractere'],
+    validate: {
+      // this only work on  CREATE and Save !!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same',
+    },
+  },
 });
+
+userSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
+  next();
+});
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
-module.exports=User;
+module.exports = User;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,9 @@ const authController = require("../controllers/authController");
 const router = express.Router();
 
 router.post('/signup',authController.signup);
+router.post('/login',authController.login);
+
+
 router
 .route('/')
 .get(userController.getAllUsers)
