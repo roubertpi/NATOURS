308558346e67f27f5a024e527871d006c397ff51
roubertpi <roubertpimentar@gmail.com>
2021-01-29 23:30:31 -0300

    167- Calculating Average Rating on Tours

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -98,6 +98,33 @@ reviewSchema.post(/^findOneAnd/, async function() {
   await this.r.constructor.calcAverageRatings(this.r.tour);
 });
 
+reviewSchema.statics.calcAverageRatings = async function(tourId) {
+
+  const stats = await this.aggregate([
+    {
+      $match:{tour: tourId}
+    },
+    {
+      $group:{
+        _id: '$tour',
+        nRating:{ $sum:1},
+        avgRating: {$avg: '$rating'}
+      }
+    }
+  ]);
+  console.log(stats);
+  await Tour.findByIdAndUpdate(tourId,{
+    ratingsQuantity: stats[0].nRating,
+    ratingsAverage:stats[0].avgRating,
+  });
+};
+
+reviewSchema.post('save',function(){
+  // this points to current review
+  this.constructor.calcAverageRatings(this.tour);
+ 
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
