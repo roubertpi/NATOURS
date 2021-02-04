bbf0989b88221a20632eebc3e50dcceccca1ffd1
roubertpi <roubertpimentar@gmail.com>
2021-02-03 23:31:15 -0300

    180 -Setting up project

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const globalErrorHandler = require('./controllers/erroController');
 const tourRouter = require('./routes/tourRoutes.js');
 const userRouter = require('./routes/userRoutes.js');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 console.log(process.env.NODE_ENV);
@@ -72,25 +73,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/',(req,res)=>{
-  res.status(200).render('base',{
-    tour: 'The Forest Hiker',
-    user: 'Jonas'
-  });
-})
 
-app.get('/overview',(req,res)=>{
-  res.status(200).render('overview',{
-    title: 'All Tours'
-  });
-});
 
-app.get('/tours',(req,res)=>{
-  res.status(200).render('overview',{
-    title: 'The Forest Hiker Tour'
-  });
-});
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
