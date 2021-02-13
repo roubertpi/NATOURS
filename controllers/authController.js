9b7756080b9e3118f8d95ea83c95d55354932cf0
roubertpi <roubertpimentar@gmail.com>
2021-02-12 23:14:29 -0300

    206 Email Template with Pug Bem vindo

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
-const sendEmail = require('../utils/email');
+const Email = require('../utils/email');
 
 // eslint-disable-next-line arrow-body-style
 const signToken = (id) => {
@@ -37,13 +37,11 @@ const createSendToken = (user, statusCode, res) => {
   });
 };
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-    passwordConfirm: req.body.passwordConfirm,
-    role: req.body.role,
-  });
+  const newUser = await User.create(req.body);
+
+  const url = `${req.protocol}://${req.get('host')}/me` ;
+  console.log(url)
+  await new Email(newUser, url).sendWelcome();
 
   createSendToken(newUser, 201, res);
 });
@@ -124,7 +122,7 @@ exports.isLoggedIn = catchAsync(async (req, res, next) => {
         req.cookies.jwt,
         process.env.JWT_SECRET
       );
-  
+
       // 2) Check if user still exists
       const currentUser = await User.findById(decoded.id);
       if (!currentUser) {
@@ -134,14 +132,14 @@ exports.isLoggedIn = catchAsync(async (req, res, next) => {
       if (currentUser.changedPasswordAfter(decoded.iat)) {
         return next();
       }
-  
+
       // There is a Logged In user
       res.locals.user= currentUser;
       return next();
     }catch(err){
       return next();
     }
-    
+
   }
   next();
 });
@@ -177,11 +175,7 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
 
   try {
-    await sendEmail({
-      email: user.email,
-      subject: 'Your password reset token (valid for 10 min)',
-      message,
-    });
+
 
     res.status(200).json({
       status: 'success',
--- utils/email.js
+++ utils/email.js
@@ -1,24 +1,57 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async options=>{
-    // 1 Create a Transporter
-    const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-          user: process.env.EMAIL_USERNAME,
-          pass: process.env.EMAIL_PASSWORD
-        }
-      });
-    // 2 Define Email options
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Roubert Ribeiro <${process.env.EMAIL_FROM}>`;
+  }
+
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      return 1;
+    }
+
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  async send(template, subject) {
+    // 1 Render Html based on a pug template
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`,
+    {
+      firstName: this.firstName,
+      url: this.url,
+      subject
+    });
+    // 2 Define email options
     const mailOptions = {
-        from: 'Roubert Ribeiro <roubertpimentar@gmail.com>' ,
-        to: options.email,
-        subject: options.subject,
-        text: options.message
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html)
+      // html
     };
-    // 3 Actually send the email
-    await transporter.sendMail(mailOptions);
+
+      await this.newTransport().sendMail(mailOptions)
+    // 3 Create a transport and send email
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the Natours Family');
+  }
 };
 
-module.exports = sendEmail;
+
+
+
